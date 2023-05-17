d11ddfca2bcf38c98f9d79cd240d4bea1aa33b45
KateePankratevaa <124819002+KateePankratevaa@users.noreply.github.com>
2023-05-17 16:50:40 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+
+namespace ИЗ2
+{
+    public partial class Form1 : Form
+    {
+        string file;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+     
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string line;
+            Queue<Member> all = new Queue<Member>();
+            StreamReader file = new StreamReader("members.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                Member m = new Member();
+                string[] t = line.Split(',');
+                m.surname = t[0];
+                m.name = t[1];
+                m.patronymic = t[2];
+                m.pol = t[3];
+                m.age = t[4];
+                m.salary = t[5];
+                all.Enqueue(m);
+            }
+            label1.Text = "мужчины\n";
+            foreach (Member member in all)
+                if (member.pol.Trim() == "муж")
+                    label1.Text = member.pol.Trim();
+            label1.Text = "\nженщины\n";
+            foreach (Member member in all)
+                if (member.pol.Trim() == "жен")
+                    label1.Text = member.pol.Trim();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Member.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ИЗ2
+{
+    class Member
+    {
+       
+        public string surname { get; set; }
+        public string name { get; set; }
+        public string patronymic { get; set; }
+        public string pol { get; set; }
+        public string age { get; set; }
+        public string salary { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0}\t{1}\t{2}\t{3}\t{4}\t{5}",
+                surname,
+                name,
+                patronymic,
+                pol,
+                age,
+                salary);
+        }
+    }
+}
